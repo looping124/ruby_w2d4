a7efc5874ce87e74b27b0b01d2a0c93a21ab9dab
Cedric Le Net <lenetcedric@gmail.com>
2021-10-07 12:01:42 +0200

    init

---
+++ lib/00_journalists.rb
@@ -0,0 +1,106 @@
+bigdata = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+
+def nb_handles (bigdata)
+  puts "Il y a #{bigdata.size} handle dans cette array."
+end
+
+def handle_le_plus_court(bigdata)
+  bigdata_croissant = bigdata.sort_by{|word| word.length}
+  puts "Le handle le plus cours est  #{bigdata_croissant[0]}"
+end
+
+def handle_de_5_caracteres(bigdata)
+  puts "Le nombre de handle de 5 caractères est : #{bigdata.count {|word| word.length == 6}}"
+end
+
+def handle_commence_par_majuscule(bigdata)
+  puts "Le nombre de handle qui commencent par une majuscule est : #{bigdata.count {|x| x[1].match?( /[[:upper:]]/ )}}"
+end
+
+def tri_ordre_alphabétique(bigdata)
+  bigdata_alpha = bigdata.sort
+  puts bigdata_alpha
+end
+
+def tri_taille_de_handle(bigdata)
+  bigdata_croissant = bigdata.sort_by{|word| word.length}
+  puts bigdata_croissant
+end
+
+def position_epenser(bigdata)
+  puts "@epenser est à la position #{bigdata.index "@epenser"} dans l'array"
+end
+
+def repartition_par_taille(bigdata)
+  15.times do |i|
+    puts "Le nombre de handle de #{i+3} caractères est : #{bigdata.count {|word| word.length == i+4}}"
+  end
+end
+
+def perform(bigdata)
+  puts"Bonjour, que souhaitez vous afficher ?"
+  puts" - Combien y a-t-il de handle dans cette array ? => tapez 1"
+  puts" - Quel est le handle le plus court de cette liste ? => tapez 2"
+  puts" - Combien y-a-t'il de handle contenant 5 caractères =>  tapez 3"
+  puts" - Combien commencent par une majuscule  ? => tapez 4"
+  puts" - Trie la liste de handle par ordre alphabétique. => tapez 5"
+  puts" - Trie la liste de handle par taille des handle  => tapez 6"
+  puts" - Quelle est la position dans l'array de la personne @epenser ? => tapez 7"
+  puts" - Une répartition des handle par taille de ces derniers => tapez 8"
+  puts" - tapez 9 pour quitter"
+
+  choix = 0
+
+  while choix != 9
+    
+    case choix
+    when 1
+      puts "choix #{choix} :"
+      nb_handles(bigdata)
+      puts "entrez un nouveau chiffre (9 pour quitter)"
+    when 2
+      puts "choix #{choix}: "
+      handle_le_plus_court(bigdata)
+      puts "entrez un nouveau chiffre (9 pour quitter)"
+    when 3
+      puts "choix #{choix} :"
+      handle_de_5_caracteres(bigdata)
+      puts "entrez un nouveau chiffre (9 pour quitter)"
+    when 4
+      puts "choix #{choix} :"
+      handle_commence_par_majuscule(bigdata)
+      puts "entrez un nouveau chiffre (9 pour quitter)"
+    when 5
+      puts "choix #{choix} :"
+      tri_ordre_alphabétique(bigdata)
+      puts "entrez un nouveau chiffre (9 pour quitter)"
+    when 6
+      puts "choix #{choix} :"
+      tri_taille_de_handle(bigdata)
+      puts "entrez un nouveau chiffre (9 pour quitter)"
+    when 7
+      puts "choix #{choix} :"
+      position_epenser(bigdata)
+      puts "entrez un nouveau chiffre (9 pour quitter)"
+    when 8
+      puts "choix #{choix} :"
+      repartition_par_taille(bigdata)
+      puts "entrez un nouveau chiffre (9 pour quitter)"
+    when 9
+      puts "choix de quitter"      
+    else
+      puts "Entrez un chiffre entre 1 et 9" 
+    end
+    choix = gets.chomp.to_i
+  end
+  puts "merci et bonne journée"
+end
+
+perform(bigdata)
+
+
+
+
+
+
